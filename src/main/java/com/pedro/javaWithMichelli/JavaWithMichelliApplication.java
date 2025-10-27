4088669d7a0f337010b8eac11eb55f7e1cf2a9a4
pedr7n <pedrotfm68@gmail.com>
2025-10-26 22:53:28 -0300

    project created

---
+++ src/main/java/com/pedro/javaWithMichelli/JavaWithMichelliApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.javaWithMichelli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWithMichelliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWithMichelliApplication.class, args);
+	}
+
+}

d390162751a0c1f3fdf25bd78c054af5cd87e264
pedr7n <pedrotfm68@gmail.com>
2025-10-27 02:08:21 -0300

    .

---
+++ src/main/java/com/pedro/javaWithMichelli/models/ProductModel.java
@@ -0,0 +1,12 @@
+package com.pedro.javaWithMichelli.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+public class ProductModel implements Serializable {
+}
